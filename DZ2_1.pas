29852e820260c28b0ba907a97ead260e56185aa3
Kotobulka <144238123+Kotobulka@users.noreply.github.com>
2023-12-01 11:00:03 +0300

    Add files via upload

---
+++ DZ2_1.pas
@@ -0,0 +1,21 @@
+﻿var i,n,sum :int64;
+    sred: real;
+    a:array of int64;
+begin
+  begin
+  readln(n);
+  SetLength(a, n);
+  writeln(length(a));
+end;
+begin
+  writeln('Введите эл-ты массива: ');
+  for i:= 0 to n-1 do 
+  begin
+    readln(a[i]);
+    sum:=sum+a[i];
+  end;
+  writeln(a);
+  sred:=sum/n;
+  writeln(sred);
+end;
+end.
